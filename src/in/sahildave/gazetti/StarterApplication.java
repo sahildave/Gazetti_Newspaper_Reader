9b5e692a6b0cf1220bd7bbc3ab1adb823b7ca347
Sahil Dave <sahildave1991@gmail.com>
2014-10-03 05:26:30 +0530

    Minor uncommenting

--- src/in/sahildave/gazetti/StarterApplication.java
+++ src/in/sahildave/gazetti/StarterApplication.java
@@ -1,19 +1,13 @@
 package in.sahildave.gazetti;
 
 import android.app.Application;
-import android.util.Log;
 import com.parse.Parse;
 import in.sahildave.gazetti.util.Constants;
 
 public class StarterApplication extends Application {
-    private static final String TAG = "MasterDetail";
-
     @Override
     public void onCreate() {
         super.onCreate();
-
         Parse.initialize(this, Constants.PARSE_APP_ID, Constants.PARSE_CLIENT_KEY);
-        // Parse.enableLocalDatastore(this);
-        Log.d(TAG, "in Application");
     }
 }
