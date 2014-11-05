745e3d2ec64d44ce3645c1c43a5d17737cbe3fd0
Sahil Dave <sahildave1991@gmail.com>
2014-11-05 20:19:32 +0530

    Clean Manifest and Constant

---
+++ src/in/sahildave/gazetti/util/Constants.java
@@ -0,0 +1,26 @@
+package in.sahildave.gazetti.util;
+
+public class Constants {
+
+    public static final String PARSE_CLIENT_KEY = "";
+    public static final String PARSE_APP_ID = "";
+    public static final String IS_FIRST_RUN = "";
+
+    public static String th_body = "";
+    public static String th_head = "";
+    public static String th_image_1 = "";
+    public static String th_image_2 = "";
+
+    public static String toi_body = "";
+    public static String toi_head = "";
+    public static String toi_image_1 = "";
+    public static String toi_image_2 = "";
+
+    public static String tie_body = "";
+    public static String tie_head = "";
+    public static String tie_image = "";
+
+    public static String fp_body = "";
+    public static String fp_head = "";
+    public static String fp_image = "";
+}
